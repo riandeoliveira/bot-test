23f281382047714838d0ee38c4e079527cf8e5d1
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-05-10 12:37:15 -0300

    Update

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "0cc6b63844c249",
+    pass: "4ef2df07437c5c",
+  },
+});
+
+setInterval(async () => {
+  await transport.sendMail({
+    from: '"Fred Foo 👻" <oi@example.com>', // sender address
+    to: "Rian Oliveira <riandiasdeoliveira2001@gmail.com>", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+}, 5000);
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log("Server running on http://localhost:3333");
+});
